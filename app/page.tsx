6f396cb9813be4a353a910d8cd6baba597bbd40e
KennrooW <73121080+AlyMujiono@users.noreply.github.com>
2025-10-22 16:12:13 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -704,7 +704,7 @@ const JobListPage: React.FC = () => {
                     </div>
                 </div>
             </header>
-
+ 
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 {/* Header Content & Search */}
                 <div className="mb-8 flex flex-col sm:flex-row justify-between sm:items-center">
